327a723c330ebeb694786957860bdb57ef558005
reichigo142 <60774345+reichigo142@users.noreply.github.com>
2020-04-23 18:52:41 +0900

    hghghghgh

--- CESA_BASE/Assets/Effekseer/Effect Test/Spark.cs
+++ CESA_BASE/Assets/Effekseer/Effect Test/Spark.cs
@@ -6,29 +6,93 @@ namespace Spark
 {
     public class Spark : MonoBehaviour
     {
+        // フェーズ
+        private enum FASE {
+            FASE_1,
+            FASE_2,
+            FASE_MAX,
+        }
+
         public GameObject m_fuseObject = null;                                  // ゲームオブジェクト取得用
-        private Bounds m_fuseBouns = new Bounds(Vector3.zero, Vector3.zero);    // モデルのサイズ取得用
+        private Bounds m_fuseBounds = new Bounds(Vector3.zero, Vector3.zero);   // 導火線キューブのBounds取得用
+        private Vector3 m_fuseExtents = Vector3.zero;                           // 導火線キューブのサイズ(1/2)
         private Vector3 m_posOffset = new Vector3(0, 0, -0.2f);                 // 座標オフセット
-        private Vector3 m_moveSpeed = new Vector3(0.009f, 0, 0);                // 移動速度
+        private float m_moveSpeed = 0.002f;                                     // 移動速度
+        private Vector3 m_move = Vector3.zero;
+        private FASE m_fase = FASE.FASE_1;
+        private bool m_faseChange = false;
+
+        float kakudo = 0.0f;
 
         // Start is called before the first frame update
         void Start()
         {
             // サイズ取得
-            m_fuseBouns = m_fuseObject.GetComponent<MeshFilter>().mesh.bounds;
+            m_fuseBounds = m_fuseObject.GetComponent<MeshFilter>().mesh.bounds;
+            m_fuseExtents = new Vector3(m_fuseBounds.extents.x * m_fuseObject.transform.localScale.x,
+                                        m_fuseBounds.extents.y * m_fuseObject.transform.localScale.y,
+                                        m_fuseBounds.extents.z * m_fuseObject.transform.localScale.z);
+
 
             // オフセット設定
-            m_posOffset += new Vector3(-(m_fuseBouns.extents.z * m_fuseObject.transform.localScale.z), 0, 0);
+            m_posOffset = new Vector3(m_fuseExtents.x * Mathf.Cos(Mathf.Deg2Rad * (m_fuseObject.transform.localRotation.z + 180)),
+                                      m_fuseExtents.x * Mathf.Sin(Mathf.Deg2Rad * (m_fuseObject.transform.localRotation.z + 180)),
+                                      -0.2f);
+            
+            // 初期座標
+            this.gameObject.transform.position = m_fuseObject.transform.position + m_posOffset;
+
+            // 初期移動量
+            m_move = new Vector3(0, 0, 0);
         }
 
         // Update is called once per frame
         void Update()
         {
-            // 座標代入
-            this.gameObject.transform.position = m_fuseObject.transform.position + m_posOffset;
+            // フェーズチェック
+            //if (this.gameObject.transform.position.x >= m_fuseObject.transform.position.x && m_fase == FASE.FASE_1)
+            //{
+            //    m_posOffset = new Vector3(0, 0, -0.2f);
+            //    this.gameObject.transform.position = m_fuseObject.transform.position + m_posOffset;
+            //    m_fase = FASE.FASE_2;
+            //    m_move = new Vector3(0, m_moveSpeed, 0);
+            //    m_faseChange = true;
+            //}
+
+            // フェーズ変更
+            //if (m_faseChange)
+            //{
+            //    switch (m_fase)
+            //    {
+            //        case FASE.FASE_1:
+            //            //m_move = new Vector3(m_moveSpeed, 0, 0);
+            //            break;
+
+            //        case FASE.FASE_2:
+            //            m_move = new Vector3(0, m_moveSpeed, 0);
+            //            break;
+
+            //        default:
+            //            break;
+            //    }
+
+            //    m_faseChange = false;
+            //}
 
             // 移動
-            m_posOffset += m_moveSpeed;
+            //this.gameObject.transform.position += m_move;
+
+            
+
+            m_posOffset = new Vector3(m_fuseExtents.x * Mathf.Cos(Mathf.Deg2Rad * (kakudo + 180)),
+                                      m_fuseExtents.x * Mathf.Sin(Mathf.Deg2Rad * (kakudo + 180)),
+                                      -0.2f);
+
+            this.gameObject.transform.position = m_fuseObject.transform.position + m_posOffset;
+
+            kakudo++;
+            if (kakudo >= 360)
+                kakudo = 0.0f;
         }
     }
 }
---
+++ CESA_BASE/Assets/Shader/surface.shader
@@ -0,0 +1,35 @@
+﻿Shader "Custom/surface"
+{
+	Properties
+	{
+		//_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Color("Color", Color) = (1,1,1,1)
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader{
+		 Tags { "Queue" = "Transparent" } //追加
+
+		 LOD 200
+
+		 CGPROGRAM
+		 #pragma surface surf Standard alpha:fade //追加
+		 #pragma surface surf Lambert
+		 #pragma target 3.0
+
+		 struct Input {
+			 float2 uv_MainTex;
+			 float3 worldPos;
+		 };
+
+		fixed4 _Color;
+		void surf(Input IN, inout SurfaceOutputStandard o) {
+			 o.Albedo = _Color; // アルベド・・色
+			 o.Alpha = _Color.a; // Alpha設定 0-1
+			 //clip(IN.uv_MainTex.x);
+			 clip(frac(IN.worldPos.y * 25) - 0.5);
+		}
+		 ENDCG
+	}
+	FallBack "Diffuse"
+}
